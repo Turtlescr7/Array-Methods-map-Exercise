dee800fc8a879f03fac194a851b74a2d8d635d63
Tito Moctezuma <tatoruami@protonmail.com>
2024-12-15 21:17:29 -0600

    code

---
+++ user.js
@@ -0,0 +1,12 @@
+const users = [
+  { firstName: "Alice", lastName: "Johnson", points: 120 },
+  { firstName: "Bob", lastName: "Smith", points: 99 },
+  { firstName: "Charlie", lastName: "Brown", points: 180 },
+];
+
+const updatedUsers = users.map((user) => ({
+  fullName: `${user.firstName} ${user.lastName}`,
+  membershipStatus: user.points > 100 ? "Premium" : "Standard",
+}));
+
+console.log(updatedUsers);
